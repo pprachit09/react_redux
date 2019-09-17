fd5abeed5afef63c3ab6b4fb6717d5ed8b9ff9a4
pprachit09 <pprachit09@gmail.com>
2019-09-17 18:48:24 +0530

    adding project action

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -7,6 +7,14 @@ const initState = {
 }
 
 const projectReducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'CREATE_PROJECT':
+      console.log(action.project)
+      break
+    default:
+      console.log('default')
+      break
+  }
   return state
 }
 
